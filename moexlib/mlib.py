3286403d0289997a0b1af7f4910de14e900ed494
Linuxboy2007 <linuxboy2007@gmail.com>
2022-11-22 14:07:49 +0400

    plot rpc

--- moexlib/mlib.py
+++ moexlib/mlib.py
@@ -9,6 +9,8 @@ import pathlib
 import pandas as pd
 import numpy as np
 from pandas_datareader.data import DataReader
+import matplotlib.pyplot as plt
+
 
 def dmei(moex_dest, start_date, end_date):
     '''
@@ -18,9 +20,9 @@ start_date - period start date
 end_date - period end date
     '''
     pathlib.Path(moex_dest).mkdir(parents=True, exist_ok=True)
-    tickers=["IMOEX","MOEX10"]
-    arguments = ['SECID','SHORTNAME','OPEN', 'CLOSE', 
-                 'HIGH', 'LOW','MEAN','RPC']
+    tickers = ["IMOEX", "MOEX10"]
+    arguments = ['SECID', 'SHORTNAME', 'OPEN', 'CLOSE',
+                 'HIGH', 'LOW', 'MEAN', 'RPC']
     for ticker in tickers:
         data = DataReader(
             ticker,
@@ -28,82 +30,80 @@ end_date - period end date
             start=start_date,
             end=end_date,
         )
-    data = data[['SECID','SHORTNAME','OPEN', 'CLOSE', 'HIGH', 'LOW']]
-    data['MEAN'] = np.array(data.iloc[:,2:5].mean(1))
+    data = data[['SECID', 'SHORTNAME', 'OPEN', 'CLOSE', 'HIGH', 'LOW']]
+    data['MEAN'] = np.array(data.iloc[:, 2:5].mean(1))
     data['RPC'] = 0
-    data.iloc[1:,7] = [ (data.iloc[i,6]-data.iloc[i-1,6])/data.iloc[i-1,6] 
-                for i in range(1,data.shape[0]) ]
+    data.iloc[1:, 7] = [(data.iloc[i, 6] - data.iloc[i - 1, 6]) / data.iloc[i - 1, 6]
+                        for i in range(1, data.shape[0])]
     dest = join(moex_dest, ticker + '.csv')
     print("Writing %s -> %s" % (ticker, moex_dest))
-    data.to_csv(dest, index_label='day', 
-                columns = arguments, encoding='utf-8-sig') 
+    data.to_csv(dest, index_label='day',
+                columns=arguments, encoding='utf-8-sig')
+
 
 def qoutes_download(instrument_tickers, data_dest, start_date, end_date):
     '''
     Dowload and processing of instrument data
     '''
     pathlib.Path(data_dest).mkdir(parents=True, exist_ok=True)
-    arguments = ['SECID','SHORTNAME','OPEN', 'CLOSE', 'HIGH', 'LOW', 'VOLUME']
+    arguments = ['SECID', 'SHORTNAME', 'OPEN', 'CLOSE', 'HIGH', 'LOW', 'VOLUME']
 
     for instrument_ticker in instrument_tickers:
         data = DataReader(instrument_ticker,
-            'moex',
-            start=start_date,
-            end= end_date,
-        )
+                          'moex',
+                          start=start_date,
+                          end=end_date,
+                          )
         dest = join(data_dest, instrument_ticker + '.csv')
         print("Writing %s -> %s" % (instrument_ticker, dest))
-        data.to_csv(dest, index_label='day', columns = arguments, encoding='utf-8-sig')
-    
+        data.to_csv(dest, index_label='day', columns=arguments, encoding='utf-8-sig')
+
 
 def qoutes_descr(data, ticker, path, data_all):
     '''
     путь для сохранения фалов
     '''
-    dest = join(path, ticker +'_processed' + '.csv')
-    dest1 = join(path,'descr_all' + '.csv')
+    dest = join(path, ticker + '_processed' + '.csv')
+    dest1 = join(path, 'descr_all' + '.csv')
     '''
     Средняя цена за день (MEAN)и 
     относительное изменением цены дня по отношению к предыдущему дню (RPC)
     '''
-    data['MEAN'] = np.array(data.iloc[:,3:6].mean(1))
+    data['MEAN'] = np.array(data.iloc[:, 3:6].mean(1))
     data['RPC'] = 0
-    data.iloc[1:,9] = [(data.iloc[i,8]-data.iloc[i-1,8])/data.iloc[i-1,8] 
-                for i in range(1,data.shape[0]) ]
+    data.iloc[1:, 9] = [(data.iloc[i, 8] - data.iloc[i - 1, 8]) / data.iloc[i - 1, 8]
+                        for i in range(1, data.shape[0])]
     print("Writing %s -> %s" % (ticker, dest))
-    data.to_csv(dest, index = False, encoding='utf-8-sig')
+    data.to_csv(dest, index=False, encoding='utf-8-sig')
     '''
     Среднее значение (Mean), с.к.о. (Std), несимметричность (Skew),
     коэффициент эксцесса (островершинность) (kurtosis),
     относительное изменение за весь период (Rpc_all).   
     '''
-    df1 = pd.DataFrame(data={'Secid':[data.iloc[0,1]],
-         'Mean': [data['MEAN'].mean()], 
-         'Std':[data['MEAN'].std()],
-         'Skew': [data['MEAN'].skew()], 
-         'kurtosis': [data['MEAN'].kurtosis()],
-         'Rpc_all': [(data.iloc[-1,8]-data.iloc[0,8])/data.iloc[0,8]]})
+    df1 = pd.DataFrame(data={'Secid': [data.iloc[0, 1]],
+                             'Mean': [data['MEAN'].mean()],
+                             'Std': [data['MEAN'].std()],
+                             'Skew': [data['MEAN'].skew()],
+                             'kurtosis': [data['MEAN'].kurtosis()],
+                             'Rpc_all': [(data.iloc[-1, 8] - data.iloc[0, 8]) / data.iloc[0, 8]]})
 
     print("Writing descr%s -> %s" % (ticker, dest))
     if pathlib.Path(dest1).is_file():
-        df1.to_csv(dest1, mode='a', header=False, index = False,
+        df1.to_csv(dest1, mode='a', header=False, index=False,
                    encoding='utf-8-sig')
     else:
-        df1.to_csv(dest1, index = False, encoding='utf-8-sig')
-    data_all[data['SHORTNAME'][0]]=data.iloc[:,9]
+        df1.to_csv(dest1, index=False, encoding='utf-8-sig')
+    data_all[data['SHORTNAME'][0]] = data.iloc[:, 9]
     return data_all
 
-def plot_rpc(rpc_dest, angle = 45):
-    df_rpc = pd.read_csv (rpc_dest,sep=',',index_col = False)
+
+def plot_rpc(rpc_dest, angle=45):
+    df_rpc = pd.read_csv(rpc_dest, sep=',', index_col=False)
     df_rpc.set_index("day", inplace=True)
     list1 = [df_rpc.index.to_list()[i][5:] for i in range(len(df_rpc.index))]
     for i in range(df_rpc.shape[1]):
-        #plt.plot(range(df_rpc.shape[0]),df_rpc.iloc[:,i],'d-')
-        #plt.rcParams['figure.figsize'] = [10,5]
-        plt.plot(list1,df_rpc.iloc[:,i],'d-')
-        plt.xticks(rotation = angle)
+        plt.plot(list1, df_rpc.iloc[:, i], 'd-')
+        plt.xticks(rotation=angle)
         plt.title(df_rpc.columns[i])
         plt.grid(True)
         plt.show()
-
-    
--- mprog.py
+++ mprog.py
@@ -6,7 +6,8 @@ Created on Tue Nov 22 12:03:57 2022
 """
 
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning) 
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from os.path import join, isdir
 import pathlib
@@ -23,54 +24,49 @@ warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 start_date = '2022-08-01'
 end_date = '2022-08-31'
 
-path = r'c:\stock data' # корневой каталог для сохрания информации
-moex_dest = join(path, start_date+ "_"+ end_date,'moexdata') # каталог для индексов МосБиржи
-data_dest= join(path, start_date+ "_"+ end_date,'data') # каталог для хранения информации о инструментах
-processed_dest = join(path, start_date+ "_"+ end_date,'processed') # каталог для хранения обработанных данных инструментов
-ticks_path = join(path, 'SECID' + '.txt') # путь для файла со списком инструментов
-
-moexl.dmei(moex_dest, start_date, end_date)
+path = r'c:\stock data'  # корневой каталог для сохрания информации
+moex_dest = join(path, start_date + "_" + end_date, 'moexdata')  # каталог для индексов МосБиржи
+data_dest = join(path, start_date + "_" + end_date, 'data')  # каталог для хранения информации о инструментах
+processed_dest = join(path, start_date + "_" + end_date,
+                      'processed')  # каталог для хранения обработанных данных инструментов
+ticks_path = join(path, 'SECID' + '.txt')  # путь для файла со списком инструментов
+if isdir(moex_dest):
+    print("MOEX indices downloaded")
+else:
+    moexl.dmei(moex_dest, start_date, end_date)
 
 '''  Список secid инструментов '''
-instrument_tickers=["ABRD","ACKO","AFKS","AFLT","AGRO","AKRN"] 
-moexl.qoutes_download(instrument_tickers, data_dest, start_date, end_date)
-# pathlib.Path(data_dest).mkdir(parents=True, exist_ok=True)
-# arguments = ['SECID','SHORTNAME','OPEN', 'CLOSE', 'HIGH', 'LOW', 'VOLUME']
+instrument_tickers = ["ABRD", "ACKO", "AFKS", "AFLT", "AGRO", "AKRN"]
+if isdir(data_dest):
+    print("Instruments indices downloaded")
+else:
+    moexl.qoutes_download(instrument_tickers, data_dest, start_date, end_date)
 
-# for instrument_ticker in instrument_tickers:
-#     data = DataReader(instrument_ticker,
-#             'moex',
-#             start=start_date,
-#             end= end_date,
-#         )
-#     dest = join(data_dest, instrument_ticker + '.csv')
-#     print("Writing %s -> %s" % (instrument_ticker, dest))
-#     data.to_csv(dest, index_label='day',
-#                 columns = arguments, encoding='utf-8-sig')
-    
-pathlib.Path(processed_dest).mkdir(parents=True, exist_ok=True)
-'''
+if isdir(processed_dest):
+    print("Instruments data processed")
+else:
+    pathlib.Path(processed_dest).mkdir(parents=True, exist_ok=True)
+    '''
 Для тестирования в переменную instrument_tickers вносим secid инструментов.
 Для анализа большего числа инструментов, secid хранятся в файле secid.txt.
 Пример получения secid
 with open(ticks_path, "r") as instrument_tickers:
    instrument_tickers = [line.rstrip() for line in instrument_tickers]
 '''
-# instrument_tickers=["ABRD","ACKO","AFKS","AFLT","AGRO","AKRN"]
-dft = pd.DataFrame()
+    dft = pd.DataFrame()
 
-for instrument_ticker in instrument_tickers:
-    df = pd.read_csv (join(data_dest, instrument_ticker + '.csv'),sep=',')
-    if df.isnull().values.any():
-        print("Empty period")
-    else:
-        dft =  moexl.qoutes_descr(df, instrument_ticker, processed_dest, dft)
-dest_all = join(processed_dest,'rpc_all' + '.csv')
-dest_corr = join(processed_dest,'correlation' + '.csv')
-dft['day'] = df['day']
-dft.set_index("day", inplace=True)
-dft.to_csv(dest_all, encoding='utf-8-sig')
-dft.corr().to_csv(dest_corr, encoding='utf-8-sig')
+    for instrument_ticker in instrument_tickers:
+        df = pd.read_csv(join(data_dest, instrument_ticker + '.csv'), sep=',')
+        if df.isnull().values.any():
+            print("Empty period")
+        else:
+            dft = moexl.qoutes_descr(df, instrument_ticker, processed_dest, dft)
+    dest_all = join(processed_dest, 'rpc_all' + '.csv')
+    dest_corr = join(processed_dest, 'correlation' + '.csv')
+    dft['day'] = df['day']
+    dft.set_index("day", inplace=True)
+    dft.to_csv(dest_all, encoding='utf-8-sig')
+    dft.corr().to_csv(dest_corr, encoding='utf-8-sig')
 
-
-
+rpc_dest = join(processed_dest,'rpc_all' + '.csv')
+moexl.plot_rpc(rpc_dest)
